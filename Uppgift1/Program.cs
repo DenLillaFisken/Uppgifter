78617488d00aef04b3c81dcd369bcad76a6e48f6
Alexandra Johansson <alexandra_johansson111@live.se>
2020-09-10 14:19:01 +0200

    1

--- Uppgift1/Program.cs
+++ Uppgift1/Program.cs
@@ -11,6 +11,10 @@ namespace Uppgift1
     {
         public static void Main(string[] args)
         {
+            //skapar ett slumpartat värde mellan -20 och 35.
+            Random rnd = new Random();
+            int temperature = rnd.Next(-20, 35);  // temperatur mellan -20 till 35 celcius.
+
             CreateHostBuilder(args).Build().Run();
         }
 
